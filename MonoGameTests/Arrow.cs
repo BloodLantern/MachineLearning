b1a06247a0a94b77227551374ffdbf878a1fd023
Maxime GRIMARD--HAMEL <m.grimardhamel@student.isartdigital.com>
2024-05-24 17:18:29 +0200

    Initial commit

---
+++ MonoGameTests/Arrow.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Network;
+using System;
+
+namespace MonoGameTests
+{
+    public class Arrow
+    {
+        public static Texture2D Texture;
+
+        private const float Velocity = 100f;
+        private static Vector2 size = new(50);
+
+        private Vector2 position;
+        private float angle;
+
+        private readonly NeuralNetwork NeuralNetwork;
+        
+        public Arrow(Vector2 position, float angle, NeuralNetwork network)
+        {
+            this.position = position;
+            this.angle = angle;
+            NeuralNetwork = network;
+        }
+
+        public void Update(GameTime gameTime, Vector2 targetPosition)
+        {
+            Vector2 targetDirection = targetPosition - position;
+            float targetAngle = (float) Math.Atan2(targetDirection.Y, targetDirection.X);
+
+            float[] result = NeuralNetwork.FeedForward([angle / MathHelper.TwoPi, targetAngle]);
+            angle = result[0] * MathHelper.TwoPi;
+
+            position += new Vector2(MathF.Cos(angle) * Velocity, MathF.Sin(angle) * Velocity) * (float) gameTime.ElapsedGameTime.TotalSeconds;
+            position = Vector2.Clamp(position, Vector2.Zero, new(Application.Instance.WindowWidth, Application.Instance.WindowHeight));
+
+            NeuralNetwork.Fitness += 1f - MathF.Abs(targetAngle - angle) / MathHelper.TwoPi;
+        }
+
+        public void Render(SpriteBatch spriteBatch)
+        {
+            spriteBatch.Draw(Texture, new(position.ToPoint(), size.ToPoint()), null, Color.White, angle, Texture.Bounds.Size.ToVector2() / 2, SpriteEffects.None, 0);
+        }
+    }
+}
---
+++ MonoGameTests/Program.cs
@@ -0,0 +1,3 @@
+﻿
+using var game = new MonoGameTests.Application();
+game.Run();
